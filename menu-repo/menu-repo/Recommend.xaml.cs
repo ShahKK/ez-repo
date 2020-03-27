127fbf798bead7d6b17cafa6971edddf57059127
ShahK <35611046+ShahKK@users.noreply.github.com>
2020-03-27 01:13:55 -0600

    Add files via upload

---
+++ menu-repo/menu-repo/Recommend.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace menu_repo
+{
+    /// <summary>
+    /// Interaction logic for Recommend.xaml
+    /// </summary>
+    public partial class Recommend : Window
+    {
+        public Recommend()
+        {
+            InitializeComponent();
+        }
+    }
+}
